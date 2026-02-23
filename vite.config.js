9b55e7be266535b6242e907cd4156f95c4df3819
emoro <esteban.moroegido@gmail.com>
2026-02-23 12:02:56 -0500

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  // Needed for GitHub Pages: https://emoro.github.io/dependency/
+  base: '/dependency/',
 })
